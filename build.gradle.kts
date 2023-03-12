9aeb4173a5cf8a3280a4a2c882094407f005c814
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-03-12 15:30:06 +0100

    Bump plugin and loader versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,20 @@
 plugins {
     java
-    id("com.github.weave-mc.weave") version "ec59a8bceb"
+    id("com.github.weave-mc.weave") version "7cc0e879eb"
 }
 
 group = "com.example"
 version = "1.0"
 
 minecraft.version.set("1.8.9")
+
+repositories {
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("com.github.Weave-MC:Weave-Loader:f736ac0323")
+}
 
 tasks.compileJava {
     options.release.set(11)
