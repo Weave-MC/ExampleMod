5211bd8447d3da4e4ab2746cfd3a57bd7ff2142d
xtrm <oss@xtrm.me>
2024-05-01 02:50:35 +0200

    🔨 chore: add logger example, improve .gitignore

--- src/main/java/com/example/mod/ExampleMod.java
+++ src/main/java/com/example/mod/ExampleMod.java
@@ -1,14 +1,18 @@
 package com.example.mod;
 
 import net.weavemc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.instrument.Instrumentation;
 
 public class ExampleMod implements ModInitializer {
+    private final Logger logger = LogManager.getLogger();
+
     @Override
     public void init() {
-        System.out.println("Initializing ExampleMod!");
+        logger.info("Hello from ExampleMod!");
     }
 
     //TODO: This will be removed
