00d32f5b3ec5bef4e4fdf2233212952be0401ab2
Candicey <13133556-Candicey@users.noreply.gitlab.com>
2026-03-25 18:57:53 +0700

    Update Weave to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
-    id("net.weavemc.gradle") version "1.0.0-PRE"
+    id("net.weavemc.gradle") version "1.1.0"
 }
 
 group = "com.example"
 version = "1.0.0"
 
-minecraft {
+weave {
     configure {
         name = "ExampleMod"
         modId = "examplemod"
         entryPoints = listOf("com.example.mod.ExampleMod")
+        hooks = listOf("com.example.mod.hook.MinecraftHook")
         mixinConfigs = listOf("examplemod.mixins.json")
         mcpMappings()
     }
@@ -17,12 +18,17 @@ minecraft {
 }
 
 repositories {
-    maven("https://repo.weavemc.dev/releases")
     maven("https://repo.spongepowered.org/maven/")
+    // Check available packages at https://gitlab.com/weave-mc/weave/-/packages/
+    maven("https://gitlab.com/api/v4/projects/80566527/packages/maven")
 }
 
 dependencies {
-    compileOnly("net.weavemc.api:common:1.0.0-b.2")
+    implementation("net.weavemc:loader:1.1.0") // For advanced modifications
+    implementation("net.weavemc:internals:1.1.0")
+    implementation("net.weavemc.api:api:1.1.0")
+    implementation("net.weavemc.api:api-v1_8:1.1.0") // For 1.8 events
+
     compileOnly("org.spongepowered:mixin:0.8.5")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://repo.weavemc.net/releases")
+        // Check available packages at https://gitlab.com/weave-mc/weave/-/packages/
+        maven("https://gitlab.com/api/v4/projects/80566527/packages/maven")
     }
 }
 
